459d0b3c0ba5f7e70d505e657089d5f6acab51e7
Bruno-OA <brunooliveiraalves78@gmail.com>
2024-02-05 13:19:17 -0300

    Criando Crud Cliente

---
+++ src/main/java/br/com/tgid/service/ClienteService.java
@@ -0,0 +1,37 @@
+package br.com.tgid.service;
+
+import br.com.tgid.entity.Cliente;
+import br.com.tgid.repository.ClienteRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> criar(Cliente cliente) {
+        clienteRepository.save(cliente);
+        return listar();
+    }
+
+    public List<Cliente> listar() {
+        Sort sort = Sort.by("nome").descending();
+        return clienteRepository.findAll(sort);
+    }
+
+    public List<Cliente> atualizar(Cliente cliente) {
+        clienteRepository.save(cliente);
+        return listar();
+    }
+
+    public List<Cliente> deletar(Long id) {
+        clienteRepository.deleteById(id);
+        return listar();
+    }
+}
