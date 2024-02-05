b53798a24ee3f0ac8cb51e8cf2c6d24ec72aaca7
Bruno-OA <brunooliveiraalves78@gmail.com>
2024-02-05 12:23:07 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/tgid/TesteJavaTgidApplication.java
@@ -0,0 +1,13 @@
+package br.com.tgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaTgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaTgidApplication.class, args);
+	}
+
+}
